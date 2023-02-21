7ff3927a24e706fa1def5a775b1c5bf201c3386d
DAH ZANNOU <jeniferdahzannou@gmail.com>
2023-02-20 22:28:33 -0800

    task4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+/*
+ *In main - A program that prints line with puts function
+ *Return:0 (Sucess)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return(0);
+}
