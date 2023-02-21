13c1ad4e4e1c21a0ee08b96e2cb436ea1cb9cdd4
DAH ZANNOU <jeniferdahzannou@gmail.com>
2023-02-20 22:51:55 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
-/*
- *In main - A program that prints line with puts function
- *Return:0 (Sucess)
- */
+/**
+ * In main - A program that prints line with puts function
+ * Return:0 (Sucess)
+*/
+
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
+
