d349f324cf921fb14a70d4d953278bb6e069e2e2
DAH ZANNOU <jeniferdahzannou@gmail.com>
2023-02-22 21:11:41 -0800

    task6

--- 0x02-functions_nested_loops/6-main.c
+++ 0x02-functions_nested_loops/6-main.c
@@ -1,13 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 
-/**     
-*main - check the code       
-*Return: Always 0.
+/**
+* main - check the code
+*
+* Return: Always 0.
 */
 int main(void)
 {
 int r;
+
 r = _abs(-1);
 printf("%d\n", r);
 r = _abs(0);
@@ -17,5 +19,4 @@ printf("%d\n", r);
 r = _abs(-98);
 printf("%d\n", r);
 return (0);
-}       
-
+}
